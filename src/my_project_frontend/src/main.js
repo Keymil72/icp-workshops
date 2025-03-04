1822b49a4c846cd3d14fa6e51f05a848a59788fe
Keymil 72 <kpodlecki7@gmail.com>
2025-03-04 18:59:56 +0000

    calculator feature

--- src/my_project_frontend/src/main.js
+++ src/my_project_frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import './index.scss';
+import './output.scss';
 import App from './App.vue';
 
 createApp(App).use(createPinia()).mount('#app');
